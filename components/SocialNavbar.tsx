853d374812616c85d21d5d17635f14193b524d26
Keegan <meddakee2000@gmail.com>
2024-07-09 20:58:35 -0400

    v0.8c

--- components/SocialNavbar.tsx
+++ components/SocialNavbar.tsx
@@ -1,6 +1,6 @@
 // Imports
 import '@/styles/SocialNavbar.css';
-import { github, instagram, resume } from '@/logic/SocialNavbar';
+import { github, twitter, resume } from '@/logic/SocialNavbar';
 
 // Interface
 interface SocialNavbarProps {
@@ -17,7 +17,7 @@ export default function SocialNavbar({spacing, size, topMargin}: SocialNavbarPro
 
         <div className = 'flexbox' style = {{marginTop: topMargin}}>
             <img className = 'button' src = '/icons/socials/github.svg' onClick = {github} width = {size}></img>
-            <img className = 'button' src = '/icons/socials/instagram.svg' onClick = {instagram} width = {size} style = {{marginLeft: spacing}}></img>
+            <img className = 'button' src = '/icons/socials/x.svg' onClick = {twitter} width = {size} style = {{marginLeft: spacing}}></img>
             <img className = 'button' src = '/icons/socials/resume.svg' onClick = {resume} width = {size} style = {{marginLeft: spacing}}></img>
         </div>
 
--- logic/SocialNavbar.ts
+++ logic/SocialNavbar.ts
@@ -4,9 +4,9 @@
 // Variables
 const linkToResume: string = 'https://docs.google.com/document/d/1qcPA0T-GLKq7H2aMuvZ3YGa_3PBgEEMYkzh9Krjxbt4/edit?usp=sharing';
 const linkToGitHub: string = 'https://github.com/rhet0r1cal';
-const linkToInstagram: string = 'https://www.instagram.com/keeganm1232/';
+const linkToTwitter: string = 'https://x.com/Rhet0r1cal_';
 
 // Functions
 export function resume(){window.open(linkToResume)};
 export function github(){window.open(linkToGitHub)};
-export function instagram(){window.open(linkToInstagram)};
+export function twitter(){window.open(linkToTwitter)};
