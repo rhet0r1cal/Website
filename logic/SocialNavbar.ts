ea30eeac578148298e9d6bb9437140e2ddb64b60
Keegan <meddakee2000@gmail.com>
2024-07-09 21:01:07 -0400

    v0.8d

--- logic/SocialNavbar.ts
+++ logic/SocialNavbar.ts
@@ -2,7 +2,7 @@
 "use client";
 
 // Variables
-const linkToResume: string = 'https://docs.google.com/document/d/1qcPA0T-GLKq7H2aMuvZ3YGa_3PBgEEMYkzh9Krjxbt4/edit?usp=sharing';
+const linkToResume: string = '/resume.pdf';
 const linkToGitHub: string = 'https://github.com/rhet0r1cal';
 const linkToTwitter: string = 'https://x.com/Rhet0r1cal_';
 
